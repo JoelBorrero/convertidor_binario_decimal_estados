628d1d3587455e0ce73448e8f76da7fdaee291b3
JoelBorrero <joeborrero13@hotmail.com>
2021-02-18 23:48:40 -0500

    first commit

--- lib/screens/myhome.dart
+++ lib/screens/myhome.dart
@@ -1,18 +1,47 @@
 import 'package:demo_app/widgets/converter.dart';
 import 'package:flutter/material.dart';
 
-class MyHome extends StatelessWidget {
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  ThemeData lightTheme = ThemeData(
+          primaryColor: Colors.teal,
+          buttonColor: Colors.teal,
+          accentColor: Colors.teal),
+      darkTheme = ThemeData(
+          primaryColor: Colors.grey[800],
+          buttonColor: Colors.blueGrey,
+          accentColor: Colors.teal[800],
+          textTheme: TextTheme(
+            bodyText2: TextStyle(color: Colors.white),
+          ),
+          scaffoldBackgroundColor: Colors.blueGrey[800]);
+
+  bool dark = true;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
+        theme: dark ? darkTheme : lightTheme,
         home: Scaffold(
             appBar: AppBar(
               title: Text('Programación Móvil 2020'),
+              actions: [
+                IconButton(
+                    icon: Icon(dark
+                        ? Icons.wb_sunny_outlined
+                        : Icons.nights_stay_outlined),
+                    onPressed: () {
+                      setState(() {
+                        dark = !dark;
+                      });
+                    })
+              ],
             ),
             body: Center(child: Converter())));
   }
--- lib/widgets/converter.dart
+++ lib/widgets/converter.dart
@@ -6,67 +6,60 @@ class Converter extends StatefulWidget {
 }
 
 class _ConverterState extends State<Converter> {
-  String _binary = "11";
-  String _decimal = "3"; // _decimal = int.parse(_binary, radix: 2).toRadixString(10);
-
-  void _onPressed() {
-  }
+  String _binary = "11", _decimal = "";
 
   @override
   Widget build(BuildContext context) {
     return Container(
-     child: 
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-               alignment: Alignment.centerLeft,
-              child: Text("Binary -> Decimal")
-              ),
-        Container(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+          Container(
+              alignment: Alignment.center, child: Text("\nBinary -> Decimal")),
+          Container(
               padding: const EdgeInsets.all(8.0),
-              alignment: Alignment.centerRight,
-              child: Text(
-                '$_binary',
-                textAlign: TextAlign.right,
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Color(int.parse("#FF5722".replaceAll('#', '0xff'))),
-                    fontSize: 35),
-              )),
-            Text("3"),
-            Row(
-                children: <Widget>[
-                  MaterialButton(
-                    child: Text("1"),
-                    onPressed: () {},
-                  ),
-                  MaterialButton(
-                    child: Text("0"),
-                    onPressed: () {},
-                  ),
-                ]),
-
-
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              child: MaterialButton(
-                  color: Color(int.parse("#0069C0".replaceAll('#', '0xff'))),
+              alignment: Alignment.center,
+              child: Text('$_binary',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color:
+                          Color(int.parse("#FF5722".replaceAll('#', '0xff'))),
+                      fontSize: 35))),
+          Center(child: Text("$_decimal", style: TextStyle(fontSize: 20))),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            FloatingActionButton(
+                child: Text("1"),
+                onPressed: () {
+                  setState(() {
+                    _binary += '1';
+                    _decimal = int.parse(_binary, radix: 2).toRadixString(10);
+                  });
+                }),
+            Text('    '),
+            FloatingActionButton(
+                child: Text("0"),
+                onPressed: () {
+                  setState(() {
+                    _binary += '0';
+                    _decimal = int.parse(_binary, radix: 2).toRadixString(10);
+                  });
+                })
+          ]),
+          Container(
+              padding: EdgeInsets.all(80),
+              child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
                   onPressed: () {
-                   
+                    setState(() {
+                      _binary = '';
+                      _decimal = '';
+                    });
                   },
                   child: Text("Reset",
-                      style: new TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white,
-                      ))),
-            ),
-          ),
-          ]),
-
-
-    );
+                      style:
+                          new TextStyle(fontSize: 20.0, color: Colors.white))))
+        ]));
   }
 }
